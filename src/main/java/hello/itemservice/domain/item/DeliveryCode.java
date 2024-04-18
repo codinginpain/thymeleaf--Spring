2be7b892cef55ebe642ded5e814d567306203ecc
Kee <codinginpain@gmail.com>
2024-04-17 23:09:06 -0400

    select box

--- src/main/java/hello/itemservice/domain/item/DeliveryCode.java
+++ src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -15,4 +15,5 @@ public class DeliveryCode {
     private String code;
     private String displayName;
 
+
 }
--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,5 +1,6 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import hello.itemservice.domain.item.ItemType;
@@ -10,18 +11,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Controller
 @RequestMapping("/form/items")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    public FormItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 
     @ModelAttribute("regions") //이렇게 ModelAttribute를 등록하면 Spring에서 자동으로 해당 controller 내 method를 call 할때마다, Model이 자동으로 담겨져있음
     public Map<String, String> regions() {
@@ -35,6 +37,15 @@ public class FormItemController {
     @ModelAttribute("itemTypes")
     public ItemType[] itemTypes() {
         return ItemType.values(); //enum의 경우 .values하면 값들을 배열로 넘겨줌
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        return deliveryCodes;
     }
 
     @GetMapping
@@ -73,7 +84,6 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
-        log.info("item.open={}", item.getOpen());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
